9740f77bff1ddb43e59407f44acbdeefd58d6555
unknown <mateusdantas.dev@gmail.com>
2024-09-25 22:33:22 -0300

    melhora legibilidade de sub-rotas

--- src/main/java/br/com/caelum/camel/RotaPedidos.java
+++ src/main/java/br/com/caelum/camel/RotaPedidos.java
@@ -16,6 +16,13 @@ public class RotaPedidos {
             @Override
             public void configure() throws Exception {
                 from("file:pedidos?delay=5s&noop=true").
+                routeId("rota-pedidos").
+                multicast().
+                    to("direct:http").
+                    to("direct:soap");
+
+                from("direct:http").
+                    routeId("rota-http").
                     setProperty("pedidoId", xpath("/pedido/id/text()")).
                     setProperty("clienteId", xpath("/pedido/pagamento/email-titular/text()")).
                     split().
@@ -27,7 +34,12 @@ public class RotaPedidos {
                     log("${id} - ${body}").
                     setHeader(Exchange.HTTP_METHOD, HttpMethods.GET).
                     setHeader(Exchange.HTTP_QUERY, simple("ebookId=${property.ebookId}&pedidoId=${property.pedidoId}&clienteId=${property.clienteId}")).
-                to("http://localhost:8080/webservices/ebook/item");
+                to("http4://localhost:8080/webservices/ebook/item");
+
+                from("direct:soap").
+                    routeId("rota-soap").
+                    setBody(constant("<envelope>teste</envelope>")).
+                to("mock:soap");
             }
         });
         context.start();
