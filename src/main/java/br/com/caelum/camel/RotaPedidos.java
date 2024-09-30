662ed98641f26eb55df2f725b517aa706b7f6a20
unknown <mateusdantas.dev@gmail.com>
2024-09-30 11:20:13 -0300

    Finaliza projeto com camel

--- src/main/java/br/com/caelum/camel/RotaPedidos.java
+++ src/main/java/br/com/caelum/camel/RotaPedidos.java
@@ -1,7 +1,9 @@
 package br.com.caelum.camel;
 
+import org.apache.activemq.camel.component.ActiveMQComponent;
 import org.apache.camel.CamelContext;
 import org.apache.camel.Exchange;
+import org.apache.camel.Processor;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.component.http4.HttpMethods;
 import org.apache.camel.impl.DefaultCamelContext;
@@ -11,15 +13,33 @@ public class RotaPedidos {
     public static void main(String[] args) throws Exception {
 
         CamelContext context = new DefaultCamelContext();
+        context.addComponent("activemq", ActiveMQComponent.activeMQComponent("tcp://localhost:61616/"));
+
         context.addRoutes(new RouteBuilder() {
 
             @Override
             public void configure() throws Exception {
-                from("file:pedidos?delay=5s&noop=true").
+                errorHandler(deadLetterChannel("activemq:queue:pedidos.DLQ")
+                        .logExhaustedMessageHistory(true)
+                        .maximumRedeliveries(3)
+                        .redeliveryDelay(2000)
+                        .onRedelivery(new Processor() {
+
+                            @Override
+                            public void process(Exchange exchange) throws Exception {
+                                int counter = (int) exchange.getIn().getHeader(Exchange.REDELIVERY_COUNTER);
+                                int max = (int) exchange.getIn().getHeader(Exchange.REDELIVERY_MAX_COUNTER);
+                                System.out.println("Redelivery" + counter + "/" + max);
+                            }
+                        })
+                );
+
+                from("activemq:queue:pedidos").
                 routeId("rota-pedidos").
+                to("validator:pedido.xsd").
                 multicast().
-                    to("direct:http").
-                    to("direct:soap");
+                    to("direct:soap").
+                    to("direct:http");
 
                 from("direct:http").
                     routeId("rota-http").
@@ -38,8 +58,10 @@ public class RotaPedidos {
 
                 from("direct:soap").
                     routeId("rota-soap").
-                    setBody(constant("<envelope>teste</envelope>")).
-                to("mock:soap");
+                    to("xslt:pedido-para-soap.xslt").
+                    log("${body}").
+                    setHeader(Exchange.CONTENT_TYPE, constant("text/xml")).
+                to("http4://localhost:8080/webservices/financeiro");
             }
         });
         context.start();
